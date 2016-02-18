184805294f5a3aeeb9a05e7e27807541c59d432d
stejmurphy <stephen.jd.murphy@gmail.com>
2016-02-18 16:10:29 +0000

    exception

---
+++ exception.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Example of using exceptions to catch errors
+ */
+
+function print_header($title, $keywords, $description )
+{
+	if (strlen($description < 40))
+		throw new Exception('A reasonable description length is required<br>');
+
+	print ("<html><head>");
+	print ("<title>$title</title>");
+	print ("<meta name=\"Keywords\" content=\"$keywords\">");
+	print ("<meta name=\"Description\" content=\"$description\">");
+	print ("</head><body>");
+}
+
+
+try{// title, keywords, description
+	print_header('My Page', 'PHP, Programming, Beer', 'description');
+}
+catch (Exception $e)
+{
+	echo($e->getMessage());
+}
+die();
