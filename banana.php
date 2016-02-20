763fa73c5bafd1249c71dc576b97bf735af3fb9a
Stephen Murphy <stephen.jd.murphy@gmail.com>
2016-02-20 14:26:35 +0000

    banana!

---
+++ banana.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$fruit_basket = array(
+    'red'       => 'apple',
+    'orange'    => 'orange',
+    'yellow'    => 'banana',
+    'green'     => 'pear'
+);
+
+echo $fruit_basket['yellow']; //prints banana
