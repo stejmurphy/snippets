278091669b9b5da8de6b49c30b9a63645b2a2e04
Stephen Murphy <stephen.jd.murphy@gmail.com>
2016-02-20 15:23:09 +0000

    example of count and error handling

--- multidimensional.php
+++ multidimensional.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL);
 //Multidimesnional array
 
 $cornucopia = array('fruit' =>
@@ -15,7 +16,21 @@ $cornucopia = array('fruit' =>
 
 );
 
-$kind_wanted = 'flower';
-$colour_wanted = 'purple';
+//$kind_wanted = 'flower';
+//$colour_wanted = 'purple';
 
-print ("The $colour_wanted $kind_wanted is " . $cornucopia[$kind_wanted][$colour_wanted]);
+//print ("The $colour_wanted $kind_wanted is " . $cornucopia[$kind_wanted][$colour_wanted]); //so not to confuse PHP4 and lower
+
+//print ("The thing we want is {$cornucopia[$kind_wanted][$colour_wanted]}"); // cleaner
+
+$kind_wanted = 'fruit';
+
+$colour_wanted ="purple";
+
+//print("The $colour_wanted $kind_wanted is" . $cornucopia[$kind_wanted][$colour_wanted]); // returns an error as there is no key of purple within fruit
+
+
+
+count($cornucopia); // will expect the first layer of the array assigned to the variable == 2
+
+count($cornucopia['fruit']); //will expect the contents of the 'fruit' array == 4
