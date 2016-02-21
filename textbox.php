14908408d3debfdc7b1e3b42b62dc58b479ef86f
Stephen Murphy <stephen.jd.murphy@gmail.com>
2016-02-21 15:59:52 +0000

    construct

--- textbox.php
+++ textbox.php
@@ -1,15 +1,20 @@
 <?php
 
+// class creation, the blueprint
 class TextBoxSimple{
-
     var $body_text = "my text";
-
+    //constructor function, always called when a new instance is created
+    function __construct($text_in)
+    {
+        $this->body_text = $text_in;
+    }
+    //Displays our data
     function display(){
         print("<table border=1><tr><td>$this->body_text</td></tr>");
     }
 }
 
 //instance
-$body = new TextBoxSimple();
+$body = new TextBoxSimple('Custom Text');
 //load method aka display the table
 $body->display();
